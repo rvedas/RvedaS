a92da91c0a48ffd59acc1fc37232b3e111fa0228
Sivaji Nandimandalam <rvedas@egsonweb.com>
2017-07-20 12:45:29 -0700

    invoke hello world script added

---
+++ scripts/invoke_helloworld.R
@@ -0,0 +1,15 @@
+library("httr")
+library(uuid)
+
+number_of_requests <- 100
+rclient_id <- 201
+
+for (i in 1:number_of_requests) {
+  ruuid <- uuid::UUIDgenerate()
+  r <-
+    httr::GET(
+      "http://24.5.246.173:9101/rvedas/lb/akamai/hw",
+      add_headers(transaction_uuid = ruuid, client_id = rclient_id)
+    )
+  
+}
